f2fff4d718fc0c7561bc919bb3c7d1696d240427
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2024-07-03 03:54:05 +0600

    Setup client side with Vite, React, TS, ShadCN

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome</h1>
+        <p>
+          This is a basic setup for a React app using TypeScript.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
