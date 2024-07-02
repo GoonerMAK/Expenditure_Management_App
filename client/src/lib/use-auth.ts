6b6df9e2d73668babeba75641c27a7c7f648c031
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2024-07-03 04:00:41 +0600

    Created custom hook to check user auth

---
+++ client/src/lib/use-auth.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useAuth = () => {
+  const [user, setUser] = useState<any>(null); 
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/api/auth/user', { withCredentials: true });
+        console.log('Testing custom hook useAuth:', response.data.user);
+
+        setUser(response.data.user);
+      } catch (error) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, []);
+
+  return { user, loading };
+};
+
+export default useAuth;
