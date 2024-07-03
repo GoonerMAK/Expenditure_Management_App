0731f58ed72a6d3f65820001aa4a522ada04c8fb
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2024-07-03 13:26:18 +0600

    Implemented logout functionality - Frontend
    - Clearing the cookie when user clicks the logout button of the header component
    - Because of Protected Routes, system automatically sends the user to the login page

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,8 +1,25 @@
 import {  User  } from 'lucide-react';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../components/ui/dropdown-menu';
 import { Button } from './ui/button';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
+import Cookies from 'js-cookie';
+
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post('http://localhost:4000/api/auth/logout', {}, { withCredentials: true });
+      Cookies.remove('jwt'); 
+      navigate('/login');
+    } catch (error) {
+      console.error('Failed to log out:', error);
+    }
+  };
+
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6"> 
       <div className="flex-1 text-center">
@@ -19,7 +36,7 @@ const Header = () => {
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Logout</DropdownMenuItem>
+          <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </header>
