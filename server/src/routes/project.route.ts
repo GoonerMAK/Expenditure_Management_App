f8e6f35c4b45dc1b0bdef419778f2676d978862e
Mashrur Ahsan (Akash) <mashrurahsan@iut-dhaka.edu>
2025-12-18 22:31:16 +0600

    updated the project routes

--- server/src/routes/project.route.ts
+++ server/src/routes/project.route.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import * as projectController from '../controllers/project.controller.js';
 import { isAuthenticated } from '../middlewares/auth.middleware.js'; 
-import { validateRequest, validateParams } from '../middlewares/validator.middleware.js';
+import { validateRequest, validateParams, validateQuery } from '../middlewares/validator.middleware.js';
 import { projectSchema, projectParamsSchema } from '../validators/project.validator.js';
+import { paginationQuerySchema } from '../validators/pagination.validator.js';
 
 export const projectRouter = Router();
 
 projectRouter.post('/projects', isAuthenticated, validateRequest(projectSchema), projectController.createProject);
 projectRouter.put('/projects/:id', isAuthenticated, validateParams(projectParamsSchema), validateRequest(projectSchema), projectController.updateProject);
 projectRouter.delete('/projects/:id', isAuthenticated, validateParams(projectParamsSchema), projectController.deleteProject);
-projectRouter.get('/projects', isAuthenticated, projectController.getAllProjects);
+projectRouter.get('/projects', isAuthenticated, validateQuery(paginationQuerySchema), projectController.getAllProjects);
 projectRouter.get('/projects/:id', isAuthenticated, validateParams(projectParamsSchema), projectController.getProjectById);
 
